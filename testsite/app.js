0ddbd3faf1fdd9002ea43f109eaa6641ad48d454
Bamidele Suarau <bsuarau@medium.com>
2015-09-07 10:29:43 -0700

    Basic framework laid

--- testsite/app.js
+++ testsite/app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// var routes = require('./routes/index');
+// var users = require('./routes/users');
 
 var app = express();
 
@@ -24,8 +24,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', routes);
-// app.use('/users', users);
 
 require('./routes')(app);
 
